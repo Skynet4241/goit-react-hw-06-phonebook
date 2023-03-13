c6283b724474378e81d5ecb6bfcb1e40f1f24995
Vladyslav Sanhinov <108656455+Skynet4241@users.noreply.github.com>
2023-03-13 21:36:37 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -15,9 +15,7 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
   key: 'root',
   version: 1,
-  whitelist: ['auth'],
   storage,
-  rehydrate: true,
 };
 
 const persistedReducer = persistReducer(
